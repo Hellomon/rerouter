6e63a95b0199164326126c371bf0643432b97426
Zack_Hsiao <eewindfly@gmail.com>
2024-03-30 01:16:31 +0800

    fix writeFile

--- index.ts
+++ index.ts
@@ -122,13 +122,16 @@ declare global {
   function targz(targetPath: string, sourcePath: string): string | 'success';
   function untargz(sourcePath: string): string | 'success';
   function xDecodeHex(hexedString: string): string | undefined;
-  function writeFile(path: string, content: string): number;
+  var writeFile: (path: string, content: string) => number;
 }
 
-const writeFileTmp = writeFile;
-// @ts-ignore
-writeFile = (path: string, content: string): number => {
-  const rtn = writeFileTmp(path, content);
-  execute(`chmod 777 ${path}`);
-  return rtn;
-};
+if (typeof writeFile === 'function') {
+  const writeFileTmp = writeFile;
+  writeFile = (path: string, content: string): number => {
+    const rtn = writeFileTmp(path, content);
+    execute(`chmod 777 ${path}`);
+    return rtn;
+  };
+} else {
+  console.log('writeFile is not defined');
+}
