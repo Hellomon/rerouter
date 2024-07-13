4d6f9940ce5e33f2992e1ed886b34a562cbaa156
ellie <bc70522@gmail.com>
2024-07-13 13:35:48 +0800

    refactor: mv ` Counter` to thirdPartyUtils

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-import { CounterConfig, DefaultCounterConfig } from './struct';
-
 export function log(...msgs: any[]) {
   const date = new Date().toLocaleString('en-US', {
     timeZone: 'Asia/Taipei',
@@ -289,60 +287,5 @@ export class Utils {
     saveImage(clonedImg, filepath);
     releaseImage(clonedImg);
     console.log(`[savePointsMarkedImage]: ${name}`);
-  }
-}
-
-// Usage:
-// Counter.counterConfig = {
-//   userId: this.config.userId,
-//   licenseId: this.config.deviceId,
-//   deviceId: this.config.licenseId,
-//   ga4Url: 'https://www.google-analytics.com/mp/collect?measurement_id=G-XXXXXXXXXX&api_secret=bXX-XXXXXXXXXX_XXXXXXX',
-// };
-// Counter.sendCounter(taskName, finishedPageName, furthurInfo);
-
-export class Counter {
-  public static counterConfig: CounterConfig = DefaultCounterConfig;
-
-  public static assign<T>(target: T, source: Partial<T>): T {
-    for (const key in source) {
-      if (Object.prototype.hasOwnProperty.call(source, key)) {
-        target[key] = source[key] as any;
-      }
-    }
-    return target;
-  }
-
-  // reference: https://ga-dev-tools.google/ga4/event-builder/, https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?client_type=gtag#send_an_event
-  public static sendCounter(eventTitle: string, eventPageName: string, furthurInfo: object) {
-    const eventHour = new Date().getHours();
-    const stringHour = (eventHour < 10 ? '0' : '') + eventHour;
-
-    const body = {
-      client_id: this.counterConfig.licenseId, // MUST ASSIGN for GA4, else will fail to collect this data
-      timestamp_micros: Date.now() * 1000,
-      events: [
-        {
-          name: eventTitle,
-          params: this.assign(
-            {
-              rerouter_page: eventPageName,
-              rerouter_task: eventTitle,
-              event_hour: stringHour,
-              engagement_time_msec: 100,
-              license_id: this.counterConfig.licenseId,
-              xr_user_id: this.counterConfig.userId,
-              device_id: this.counterConfig.deviceId,
-            },
-            furthurInfo
-          ),
-        },
-      ],
-    };
-
-    httpClient('POST', this.counterConfig.ga4Url, JSON.stringify(body), {
-      'Content-Type': 'application/json',
-    });
-    Utils.log(`Sending counter with ${JSON.stringify(this.counterConfig)}, ${JSON.stringify(body)}, task: ${eventTitle}, page: ${eventPageName}`);
   }
 }
